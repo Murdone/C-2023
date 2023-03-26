b994c0aafc47e1523bb35c4e96df7819f6d0f7ef
Percibal Vasquez <vasquez.percibal@gmail.com>
2023-03-26 20:30:26 -0300

    FinishmultilevelCrudFormcountrisestatesandcities

--- Sales/Sales.API/Controllers/CountriesController.cs
+++ Sales/Sales.API/Controllers/CountriesController.cs
@@ -62,7 +62,10 @@ namespace Sales.API.Controllers
         [HttpGet("{id:int}")]
         public async Task<ActionResult> Get(int id)
         {
-            var country = await _context.Countries.FirstOrDefaultAsync(x => x.Id == id);
+            var country = await _context.Countries.
+                Include(s => s.States!).
+                ThenInclude(c => c.Cities)
+                .FirstOrDefaultAsync(x => x.Id == id);
             if (country is null)
             {
                 return NotFound();
